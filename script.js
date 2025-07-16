adbbf18b452ab00d2ab4d150f12448fdde25441e
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-07-16 11:31:35 -0300

    Pausando o temporizador - clearInterval

--- script.js
+++ script.js
@@ -62,14 +62,28 @@ function alterarContexto(contexto) {
 }
 
 const contagemRegressiva = () =>{
-  //iniciar
+  if (tempoDecorridoEmSegundos <= 0){
+    zerar()
+    alert('Tempo finalizado!')
+    return
+  }
+    
   tempoDecorridoEmSegundos -=1
   console.log('Temporizador: ' + tempoDecorridoEmSegundos)
 
 }
 
-startPauseBtn.addEventListener('click', contagemRegressiva);
+startPauseBtn.addEventListener('click', iniciarOuPausar);
 
-function iniciar() {
+function iniciarOuPausar() {
+  if (intervaloId) {
+    zerar()
+    return
+  }
   intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+  clearInterval(intervaloId)
+  intervaloId = null
 }
