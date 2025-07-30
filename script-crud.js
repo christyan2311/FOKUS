46187407a1a937b7d0db71e2b2a3ce31f35c4a3d
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-07-30 17:42:19 -0300

    Atualizando o localStorange

--- script-crud.js
+++ script-crud.js
@@ -7,6 +7,10 @@ const ulTarefas = document.querySelector('.app__section-task-list')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
+function atualizarTarefas() {
+     localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
     li.classList.add('app__section-task-list-item')
@@ -28,8 +32,10 @@ function criarElementoTarefa(tarefa) {
     botao.classList.add('app_button-edit')
 
     botao.onclick = () => {
-        const novaDescricao = prompt("Qual é o novo nome da tarefa?")
+        const novaDescricao = prompt("Qual é o novo nome da tarefa")
         novaDescricao.textContent = novaDescricao
+        tarefa.descricao = novaDescricao
+        atualizarTarefas()
     }
 
     const imagemBotao = document.createElement('img')
@@ -55,7 +61,7 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa)
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    atualizarTarefas()
     textarea.value = ''
     formAdicionarTarefa.classList.add('hidden')
 })
